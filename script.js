5ca0159c8830429913b7f471939176a97de4f35f
handesaurabh <handesaurabh2004@gmail.com>
2025-09-05 11:48:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  let freq = {};
+    for (let char of str) {
+        freq[char] = (freq[char] || 0) + 1;
+    }
+    for (let char of str) {
+        if (freq[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
